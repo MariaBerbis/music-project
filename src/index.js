421760605448b095d0964c34dad210cb2111a5bd
MariaBerbis <maria.berbis.m@gmail.com>
2024-01-08 16:56:02 +0000

    Divided JS in a separated file

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function listen() {
+  let name = prompt("What's your name?");
+  let email = prompt("What's your email address?");
+  let musicType = prompt("What's your favourite music style?");
+  musicType = musicType.toLowerCase();
+  musicType = musicType.trim();
+
+  if (musicType === "rnb" || musicType === "r&b") {
+    alert("Thank you " + name + "! Keep listening to R&B music");
+  } else {
+    alert(
+      "Thank you " +
+        name +
+        "! Good that you like  " +
+        musicType +
+        " music, only try some R&B! 😁"
+    );
+  }
+}
+
+let listenButton = document.querySelector(".listen-button");
+listenButton.addEventListener("click", listen);
